ca222056a7a9a455f0839d406a7201a644278e0f
Moises Gonzalez <moises109811@gmail.com>
2022-11-30 23:30:20 -0800

    Add files via upload

---
+++ src/App-components/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+export const Header = () => {
+    return (
+        <div>
+            <header>
+            <span class="First-letter">A</span>lejandro <span class="First-letter">M</span>ercado
+            </header>
+        </div>
+    )
+}
+
